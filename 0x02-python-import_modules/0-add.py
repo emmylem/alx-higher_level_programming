99ad3941653244d0cc74de5f5410ac66bd6c3886
emmylem <benjaminosondu55@gmail.com>
2023-01-05 16:39:23 +0000

    0-add.py, README, add_0.pydj d/ hdjh' hdle e4 gr g g /b b g/b'b,tn
    n
    '.n
    
    n
    '.n
    '.n
    g
    n
    r
    ukwviuhweikfk~cdflf

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    from add_0 import add
+
+    x = add(1, 2)
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, x))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+0x02 Modules PYTHON task ALX
