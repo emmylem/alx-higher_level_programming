977714ffca61ea05912049bca9e775dca2e4afbf
emmylemk9 <benjaminosondu55@gmail.com>
2023-01-24 04:55:45 -0800

    4-square.py setting and getting property

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
         """Initialize a new square.
         Args:
             size (int): The size of the new square."""
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
