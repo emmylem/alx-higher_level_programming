c3008367f2722d7406ad5585179e01063b4a5217
emmylem <benjaminosondu55@gmail.com>
2023-01-30 12:37:31 +0000

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,7 +44,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-
-
-my = Rectangle(0, 0)
-print("Area: {} - Perimeter: {}".format(my.area(), my.perimeter()))
