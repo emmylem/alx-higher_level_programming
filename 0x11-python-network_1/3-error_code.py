a2dc4f7601364110d67630fcb2ca0ddaa50a3014
emmylem <benjaminosondu55@gmail.com>
2023-04-28 12:14:11 +0000

    task 3 & 4

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a Request to a given url"""
+
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print(body.decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
